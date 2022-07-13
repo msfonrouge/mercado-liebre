7962e1708a814b35b72e608101865a7f64077cb3
Maximo Segundo Fonrouge <msfonrouge@gmail.com>
2022-07-13 07:01:52 -0300

    Cambios Heroku

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log('Servidor funcionando');
 });
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
+    res.sendFile(path.join(__dirname + '/views/home.html'));
 });
 
 app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
+    res.sendFile(path.join(__dirname + '/views/register.html'));
 });
 
 app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+    res.sendFile(path.join(__dirname + '/views/login.html'));
 });
